ff59187e9f5a38a80ee4633b79b31329a34cc9b9
Mohan <117554773+immoh4n@users.noreply.github.com>
2024-03-15 10:41:33 +0530

    Update finalapp.py

--- finalapp.py
+++ finalapp.py
@@ -13,15 +13,18 @@ st.set_page_config(page_title="House price prediction",
 # working_dir = os.path.dirname(os.path.abspath(__file__))
 # with open('house_price_model.sav', 'rb') as file:
 #     house_price_model = pickle.load(file)
-karapakkam=1
-adyar=2
-chrompet=3
-velachery=4
-kk_nagar=5
-anna_nagar=6
-t_nagar=7
+# Define the area options and their corresponding integer representations
+area_mapping = {
+    'karapakkam': 1,
+    'adyar': 2,
+    'chrompet': 3,
+    'velachery': 4,
+    'kk_nagar': 5,
+    'anna_nagar': 6,
+    't_nagar': 7
+}
 
-area_options = ['karapakkam','adyar','chrompet','velachery','kk_nagar','anna_nagar','t_nagar']
+
 # Sidebar for user input
 with st.sidebar:
     st.title('House Price Prediction')
@@ -29,7 +32,7 @@ with st.sidebar:
 
     col1, = st.columns(1)
     with col1:
-        area = st.selectbox('Area',area_options)
+        area = st.selectbox('Area',list(area_mapping.keys()))
         sqft = st.text_input('Square Footage')
         dist_main = st.text_input('Distance to main road')
         bedrooms = st.text_input('Number of Bedrooms')
@@ -41,7 +44,7 @@ with st.sidebar:
 
 # Convert input to appropriate data types
 try:
-    area = float(area)
+    area = area_mapping[area]
     sqft = float(sqft)
     dist_main = float(dist_main)
     bedrooms = int(bedrooms)
